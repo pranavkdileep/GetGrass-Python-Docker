f2bc08f68433c4084fae273ecc5f1ec34a3c11a5
PRANAV K DILEEP <91619846+pranavkdileep@users.noreply.github.com>
2024-06-28 22:15:28 +0530

    Create herin.py

---
+++ herin.py
@@ -0,0 +1,80 @@
+import asyncio
+import websockets
+import random
+import ssl
+import json
+import time
+import uuid
+from loguru import logger
+from fake_useragent import UserAgent
+
+user_agent = UserAgent()
+random_user_agent = user_agent.random
+
+
+async def connect_to_wss(user_id):
+    device_id = str(uuid.uuid4())
+    logger.info(device_id)
+    while True:
+        try:
+            await asyncio.sleep(random.randint(1, 10) / 10)
+            custom_headers = {
+                "User-Agent": random_user_agent
+            }
+            ssl_context = ssl.create_default_context()
+            ssl_context.check_hostname = False
+            ssl_context.verify_mode = ssl.CERT_NONE
+            uri = "wss://proxy.wynd.network:4650/"
+            server_hostname = "proxy.wynd.network"
+
+            async with websockets.connect(uri, ssl=ssl_context, extra_headers=custom_headers) as websocket:
+                async def send_ping():
+                    while True:
+                        send_message = json.dumps(
+                            {"id": str(uuid.uuid4()), "version": "1.0.0", "action": "PING", "data": {}})
+                        logger.debug(send_message)
+                        await websocket.send(send_message)
+                        await asyncio.sleep(20)
+
+                # asyncio.create_task(send_http_request_every_10_seconds(socks5_proxy, device_id))
+                await asyncio.sleep(1)
+                asyncio.create_task(send_ping())
+
+                while True:
+                    response = await websocket.recv()
+                    message = json.loads(response)
+                    logger.info(message)
+                    if message.get("action") == "AUTH":
+                        auth_response = {
+                            "id": message["id"],
+                            "origin_action": "AUTH",
+                            "result": {
+                                "browser_id": device_id,
+                                "user_id": user_id,
+                                "user_agent": custom_headers['User-Agent'],
+                                "timestamp": int(time.time()),
+                                "device_type": "extension",
+                                "version": "3.3.2"
+                            }
+                        }
+                        logger.debug(auth_response)
+                        await websocket.send(json.dumps(auth_response))
+
+                    elif message.get("action") == "PONG":
+                        pong_response = {"id": message["id"], "origin_action": "PONG"}
+                        logger.debug(pong_response)
+                        await websocket.send(json.dumps(pong_response))
+        except Exception as e:
+            logger.error(e)
+
+
+async def main():
+    #find user_id on the site in conlose localStorage.getItem('userId') (if you can't get it, write allow pasting)
+    _user_id = "2iW3yodlWUtB7l5IgUjErkS5fxm"
+
+    await connect_to_wss(_user_id)
+
+
+if __name__ == '__main__':
+    #letsgo
+    asyncio.run(main())
